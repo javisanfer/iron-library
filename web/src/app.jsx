22c7acf21c163aac6211859c103b179904c587e1
luna <lunapadilla.8@gmail.com>
2024-12-15 16:02:18 +0100

    feat: add Home

--- web/src/app.jsx
+++ web/src/app.jsx
@@ -1,14 +1,17 @@
-import Carrousel from "./components/layouts/carrousel/carrousel"
-import Navbar from "./components/ui/navbar/navbar"
-
+import Carrousel from "./components/layouts/carrousel/carrousel";
+import Navbar from "./components/ui/navbar/navbar";
+import Home from "./pages/home";
+import { Routes, Route } from "react-router-dom";
 function App() {
-
   return (
     <>
-    <Navbar />
-    <Carrousel/>
+      <Navbar />
+      <Carrousel />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
